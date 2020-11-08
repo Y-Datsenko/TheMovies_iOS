e26241bb0514b7ece4f496d81e308376d74c1fc5
Y-Datsenko <yevhenii.datsenko@gmail.com>
2020-11-08 10:20:03 +0200

    initial commit

---
+++ TheMovies/TheMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMoviesApp.swift
+//  TheMovies
+//
+//  Created by Yevhenii Datsenko on 08.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct TheMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
